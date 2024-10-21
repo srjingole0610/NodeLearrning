2204331780f3e1a97305cf1b914f8c2ce47202c2
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-21 11:21:17 +0530

    Added new package

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const _loadash = require("lodash");
 
-const items = [1, [2, [3, [4]]]];
+const items = [1, [2, [3, [4, 5]]]];
 
 const newItems = _loadash.flattenDeep(items);
 
 console.log(newItems);
+console.log("Hello World");
+console.log("Hello Nodemon");
